31bf152328a26c762e3aa42ab16be24517c27a29
Joe Gasiorek <joe.gasiorek@gmail.com>
2022-07-08 22:41:59 +0000

    first

---
+++ Shared/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  Shared
+//
+//  Created by ec2-user on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
