14137e4533cb939a3a625ca87737922c082928d7
Joe Gasiorek <joe.gasiorek@gmail.com>
2022-07-09 08:46:16 +0000

    remove

--- Tests iOS/Tests_iOS.swift
+++ Tests iOS/Tests_iOS.swift
@@ -34,12 +34,12 @@ class Tests_iOS: XCTestCase {
         XCTAssertEqual(["hi", "bye"].randomElement(), "hi")
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+//    func testLaunchPerformance() throws {
+//        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+//            // This measures how long it takes to launch your application.
+//            measure(metrics: [XCTApplicationLaunchMetric()]) {
+//                XCUIApplication().launch()
+//            }
+//        }
+//    }
 }
